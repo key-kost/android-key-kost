b0d90e6fd44447e1f6dd4c52a293e31a4111c1b0
igprad <ign.aldi.pradana@gmail.com>
2017-05-09 19:50:34 +0700

    Add init for Kelola Penyewa Kos ( Show , Search, Add , Update, Delete)

---
+++ Key-Kost/app/src/main/java/com/tna_team/key_kost/KelolaPenyewa.java
@@ -0,0 +1,13 @@
+package com.tna_team.key_kost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KelolaPenyewa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelola_penyewa);
+    }
+}
