d21a3990cd0c573aa8533769d97b2a1113e38c84
igprad <ign.aldi.pradana@gmail.com>
2017-05-28 17:01:15 +0700

    Added Volley to get All Pegawai

--- Key-Kost/app/src/main/java/com/tna_team/key_kost/KelolaPenyewa.java
+++ Key-Kost/app/src/main/java/com/tna_team/key_kost/KelolaPenyewa.java
@@ -1,8 +1,9 @@
 package com.tna_team.key_kost;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,12 +12,31 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.tna_team.key_kost.model.Penyewa;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class KelolaPenyewa extends AppCompatActivity {
     private Button btnTambahPenyewaKos;
     private ListView listView;
     private EditText edSearch;
+    private List<Penyewa> listOfPenyewa = new ArrayList<>();
+    private ProgressDialog progressDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +48,9 @@ public class KelolaPenyewa extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listPenyewa);
         edSearch = (EditText) findViewById(R.id.edSearch);
 
-        setDummyListView();
+        progressDialog = new ProgressDialog(KelolaPenyewa.this);
+        getAllPenyewa();
+//        setDummyListView();
 
         btnTambahPenyewaKos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,8 +78,72 @@ public class KelolaPenyewa extends AppCompatActivity {
         listView.setAdapter(new ArrayAdapter<String>(getApplication(),android.R.layout.simple_list_item_1,dummySet));
     }
 
+    public void setListPenyewa(){
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < listOfPenyewa.size(); i++) {
+            result.add(listOfPenyewa.get(i).getFullName());
+        }
+        listView.setAdapter(new ArrayAdapter<String>(getApplication(),android.R.layout.simple_list_item_1,result));
+    }
+
     @Override
     public void onBackPressed() {
         Toast.makeText(getApplicationContext(),"You cannot back to login",Toast.LENGTH_SHORT).show();
+    }
+
+    public void getAllPenyewa(){
+        RequestQueue requestQueue = Volley.newRequestQueue(KelolaPenyewa.this);
+        StringRequest request = new StringRequest(Method.GET, "http://ditoraharjo.co/keykost/api/v1/penyewa?kost_id=" + HomeActivity.userLogin.getKost_id(),
+                new Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray result = new JSONArray(response);
+                            for (int i = 0; i < result.length(); i++) {
+                                Penyewa penyewa = new Penyewa();
+                                JSONObject indexObject = result.getJSONObject(i);
+                                penyewa.setEmail(indexObject.getString("email"));
+                                penyewa.setAlamat(indexObject.getString("alamat"));
+                                penyewa.setExpDate(indexObject.getString("exp_date"));
+                                penyewa.setFullName(indexObject.getString("fullname"));
+                                penyewa.setId(indexObject.getString("id"));;
+                                penyewa.setImage(indexObject.getString("image"));
+                                penyewa.setJenisKelamin(indexObject.getString("jenis_kelamin"));
+                                penyewa.setNoKamar(indexObject.getString("no_kamar"));
+                                penyewa.setRegisterDate(indexObject.getString("register_date"));
+                                penyewa.setRfidId(indexObject.getString("rfid_id"));
+                                penyewa.setTelp(indexObject.getString("telp"));
+
+                                listOfPenyewa.add(penyewa);
+                            }
+                            Toast.makeText(KelolaPenyewa.this,"Success Get Data",Toast.LENGTH_SHORT)
+                                    .show();
+                            setListPenyewa();
+                            progressDialog.dismiss();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(KelolaPenyewa.this,"Sorry, network error, please fix your connection first",Toast.LENGTH_SHORT)
+                                    .show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                },
+                new ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(KelolaPenyewa.this,"Failed to connect to server",Toast.LENGTH_SHORT)
+                                .show();
+                        progressDialog.dismiss();
+                    }
+                }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("Accept","application/json");
+                return params;
+            }
+        };
+        progressDialog.show();
+        requestQueue.add(request);
     }
 }
---
+++ Key-Kost/app/src/main/java/com/tna_team/key_kost/model/Penyewa.java
@@ -0,0 +1,98 @@
+package com.tna_team.key_kost.model;
+
+/**
+ * Created by D on 28-May-17.
+ */
+
+public class Penyewa {
+    private String id,rfidId,fullName,email,noKamar,jenisKelamin,telp,alamat,image,registerDate,expDate;
+    public Penyewa(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRfidId() {
+        return rfidId;
+    }
+
+    public void setRfidId(String rfidId) {
+        this.rfidId = rfidId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNoKamar() {
+        return noKamar;
+    }
+
+    public void setNoKamar(String noKamar) {
+        this.noKamar = noKamar;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getTelp() {
+        return telp;
+    }
+
+    public void setTelp(String telp) {
+        this.telp = telp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(String registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+}
